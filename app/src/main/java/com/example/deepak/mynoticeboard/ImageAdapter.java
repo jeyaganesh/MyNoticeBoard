210bcfc3c82c28fd72d0301b835802fd942fadc9
deepak <m.deepakeee1@gmail.com>
2017-04-01 13:28:56 +0530

    Adding All Files

---
+++ app/src/main/java/com/example/deepak/mynoticeboard/ImageAdapter.java
@@ -0,0 +1,75 @@
+package com.example.deepak.mynoticeboard;
+
+/**
+ * Created by deepak on 31/3/17.
+ */
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class ImageAdapter extends PagerAdapter {
+    // Declare Variables
+    Context context;
+
+    int[] flag;
+    LayoutInflater inflater;
+
+    public ImageAdapter(Context context, int[] flag) {
+        this.context = context;
+     ;
+        this.flag = flag;
+    }
+
+    @Override
+    public int getCount() {
+        return flag.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((RelativeLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        // Declare Variables
+
+        ImageView imgflag;
+
+        inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = inflater.inflate(R.layout.viewpager_image, container,
+                false);
+
+
+
+
+
+
+        // Locate the ImageView in viewpager_item.xml
+        imgflag = (ImageView) itemView.findViewById(R.id.flag);
+        // Capture position and set to the ImageView
+        imgflag.setImageResource(flag[position]);
+
+        // Add viewpager_item.xml to ViewPager
+        ((ViewPager) container).addView(itemView);
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        // Remove viewpager_item.xml from ViewPager
+        ((ViewPager) container).removeView((RelativeLayout) object);
+
+    }
+}
+
--- app/src/main/java/com/example/deepak/mynoticeboard/Main4Activity.java
+++ app/src/main/java/com/example/deepak/mynoticeboard/Main4Activity.java
@@ -1,16 +1,29 @@
 package com.example.deepak.mynoticeboard;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class Main4Activity extends AppCompatActivity {
-
+    ViewPager viewPager;
+    int image[];
+    ImageAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        ImageView iv=(ImageView)(findViewById(R.id.flag));
-        iv.setImageResource(R.drawable.ee);
+        ;
+        image = new int[] { R.drawable.day1, R.drawable.day2,
+                R.drawable.day3, R.drawable.day4,
+                R.drawable.day5, R.drawable.day6, R.drawable.day7
+                };
+
+        // Locate the ViewPager in viewpager_main.xml
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        // Pass results to ViewPagerAdapter Class
+        adapter = new ImageAdapter(Main4Activity.this,image);
+        // Binds the Adapter to the ViewPager
+        viewPager.setAdapter(adapter);
     }
 }
